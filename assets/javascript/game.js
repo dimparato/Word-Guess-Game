1ca9cec859b7f4a11ad8a8ec2d16078118fe92e6
dimparato <ponticremator@gmail.com>
2019-09-11 23:53:35 -0400

    Still Shit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,49 @@
 var words = ["meh", "stegosaurus", "echo", "defenestration", "flatulence", "proctology"];
 
 function Game() {
-    var word = words[Math.floor(Math.random()*words.length)];
-    console.log(word)
-    var hidden = "";
-    var overallHits = 0;
-    var wrongAnswers=0;
+    var word;
+    var overallHits=0;
+    var wins=0;
+    var losses=0;
+    var hidden = [];
 
-    for (let i=0; i < word.length; i++){
-        hidden = hidden + "_";
+    function resetGame(){
+        var mistakes = 0;
+
+        word = words[Math.floor(Math.random()*words.length)];
+        for (let i=0; i < word.length; i++){
+            hidden.push("_");
+        }
+        document.getElementById("currentWord").textContent = hidden.join("");
+        document.getElementById("gamePrompt").textContent = "Guess a letter! Mess up 5 times and you lose!";
+        document.getElementById("mistakes").textContent = "Mistakes = " + mistakes;
+        document.getElementById("wins").textContent = "Wins = " + wins;
+        document.getElementById("losses").textContent = "Losses = " + losses;
     }
-
-    document.getElementById("currentWord").textContent = hidden;
-
-    
-        document.getElementById("gamePrompt").textContent = "Guess a letter!";
-        window.addEventListener ("onkeydown", function(guess){
-            var hits = 0;
-            for (let i=0; i < word.length; i++) {
-                if (guess.key===word[i]){
-                    hidden[i] = guess.key;
-                    hits++;
-                    overallHits++;
-                }
+    resetGame();
+    window.addEventListener ("keydown", function(guess){
+        for (let i=0; i < word.length; i++) {
+            var hits=0
+            if (guess.key===word[i]){
+                hidden[i] = guess.key;
+                hits++;
+                overallHits++;
             }
-            document.getElementById("currentWord").textContent = hidden;
-            if (hits > 0) {
-                document.getElementById("gamePrompt").textContent = "Nice! Try again!";
-            }
-            else {
-                document.getElementById("gamePrompt").textContent = "Wrong! Try again!";
-                wrongAnswers++;
-            }
-        });
-    
-    return wrongAnswers;
+        }
+        document.getElementById("currentWord").textContent = hidden.join("");
+        if (overallHits > word.length) {
+            document.getElementById("gamePrompt").textContent = "Congrats! A winner is you! :-)";
+            losses++;
+            resetGame();
+        }
+        else if (hits==0 && wrongAnswers<5) {
+            document.getElementById("gamePrompt").textContent = "Keep Trying!";
+            mistakes++;
+        }
+        else {
+            document.getElementById("gamePrompt").textContent = "Sorry! Game over! :-(";
+            wins++;
+            resetGame();
+        }
+    });
 }
